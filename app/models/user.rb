15fcd93733ca52c591b3f681feb82c2794f9f51e
Ramon3162 <ramonomondi@gmail.com>
2019-08-05 22:40:41 +0300

    ch(authentication): tokenise the api - configure helper gems - write create and login endpoints - authenticate using jwt

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  before_save :downcase_email
+  has_secure_password
+
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: /@/
+
+  def downcase_email
+    self.email = email.delete(' ').downcase
+  end
+end
